56388bfa4881acfb671196b6c62f62649dd4668d
shrishti-yogaraj <shrishti@thatworks.studio>
2025-09-26 23:57:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,8 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             document.documentElement.setAttribute('data-theme', newTheme);
             localStorage.setItem('theme', newTheme);
-        });
-    }
+        }
 themeToggle.addEventListener('click', () => {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
